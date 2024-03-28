63a9e8f09f89d6954e7f1cd83eec31406ab87079
coderlo119 <doantogiabao@gmail.com>
2024-03-28 13:05:00 +0700

    Add project files.

---
+++ DACS/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DACS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public AdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> CreateAdminAccount()
+        {
+            if (!await _roleManager.RoleExistsAsync("Admin"))
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            var user = new IdentityUser
+            {
+                UserName = "doantogiabao@gmail.com",
+                Email = "doantogiabao@gmail.com"
+            };
+            var rusult = await _userManager.CreateAsync(user, "Hutech@123");
+            if (rusult.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+                return Content("Adimin account created successfully!");
+            }
+            return BadRequest("Failed to create admin account");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DACS/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DACS.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DACS.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
