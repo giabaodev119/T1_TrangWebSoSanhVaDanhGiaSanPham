79f4f05bc91cfdf26a291e6d9e8cd42dd4d0bc2b
coderlo119 <doantogiabao@gmail.com>
2024-04-15 22:05:26 +0700

    chỉnh một tí layout

--- DACS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DACS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -129,7 +129,6 @@ namespace DACS.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
-
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
