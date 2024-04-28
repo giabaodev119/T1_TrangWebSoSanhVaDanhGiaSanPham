0be0f4b6d1a187d1f6c8ed3a82902e85d4a7ef4e
phananhkhoa2003 <tkcuakhoachamchi@gmail.com>
2024-04-29 02:06:59 +0700

    dang them comment box

---
+++ DACS/Models/EF/ProductComment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DACS.Models.EF
+{
+    public class ProductComment : CommonAbstract
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [StringLength(150, ErrorMessage = "Không được vượt quá 150 ký tự")]
+        public int ProductId { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ DACS/Models/EF/ProductDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace DACS.Models.EF
+{
+    public class ProductDetail
+    {
+        private Product product { get; set; }
+        private List<ProductComment> comments { get; set; }
+    }
+}
