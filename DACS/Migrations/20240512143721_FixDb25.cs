579168a6d3225681ee07ab4f7a7a289ece84b002
coderlo119 <doantogiabao@gmail.com>
2024-05-12 21:52:30 +0700

    thêm full name

---
+++ DACS/Migrations/20240512143721_FixDb25.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DACS.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDb25 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- DACS/Models/EF/ApplicationUser.cs
+++ DACS/Models/EF/ApplicationUser.cs
@@ -6,8 +6,6 @@ namespace DACS.Models
     public class ApplicationUser : IdentityUser
     {
      
-        public string? FullName { get; set; }
-        public string? Address { get; set; }
-        public string? Age { get; set; }
+        public string FullName { get; set; }
     }
 }
