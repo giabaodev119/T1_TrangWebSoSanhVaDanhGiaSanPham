d2ae2417ab8a22fe827919e4c72603b8344df85a
coderlo119 <doantogiabao@gmail.com>
2024-05-26 01:50:45 +0700

    chinh nua

--- DACS/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ DACS/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -153,22 +153,6 @@ namespace DACS.Areas.Identity.Pages.Account
 
 
 
-        private ApplicationUser CreateUser()
-        {
-            try
-            {
-                return Activator.CreateInstance<ApplicationUser>();
-            }
-            catch
-            {
-
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
-                    $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
-            }
-        }
-
-
 
         public async Task<IActionResult> OnPostConfirmationAsync(string returnUrl = null)
         {
