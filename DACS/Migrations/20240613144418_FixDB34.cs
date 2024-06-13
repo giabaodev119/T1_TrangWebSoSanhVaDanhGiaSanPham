eb43faf0b62aea587d4b700a62404602c7775eaa
coderlo119 <doantogiabao@gmail.com>
2024-06-13 22:23:35 +0700

    sửa giao diện

---
+++ DACS/Migrations/20240613144418_FixDB34.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DACS.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDB34 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Advs");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Advs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Advs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Advs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Advs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Advs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- DACS/Models/EF/Adv.cs
+++ DACS/Models/EF/Adv.cs
@@ -10,10 +10,9 @@ namespace DACS.Models.EF
         public int Id { get; set; }
         [Required(ErrorMessage = "Tên không được để trống")]
         [StringLength(150, ErrorMessage = "Không được vượt quá 150 ký tự")]
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public string? ImageUrl { get; set; }
-        public string Link { get; set; }
-        public int Type { get; set; }
+
     }
 }
